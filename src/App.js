96848088ba20c41a8f350e198ee9b05d126e73a9
Adrian Ciochon <34275@student.pwsztar.edu.pl>
2023-12-07 09:31:08 +0100

    Poprawki App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navigate, Route, Routes} from "react-router-dom";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Register from "./components/Register";
+import AddFilm from "./components/AddFilm";
+import React from "react";
+import Details from "./components/Details";
+import {decodeToken, isExpired} from "react-jwt";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home/>}></Route>
+                <Route path="signin" element={!isExpired(localStorage.getItem('token')) ? <Navigate replace to="/"/> : <Login/>} />
+                <Route path="signup" element={!isExpired(localStorage.getItem('token')) ? <Navigate replace to="/"/> : <Register/>} />
+                <Route path="/add" element={<AddFilm/>}></Route>
+                <Route path="/details" element={<Details/>}></Route>
+            </Routes>
+        </>
+    );
 }
-
 export default App;
