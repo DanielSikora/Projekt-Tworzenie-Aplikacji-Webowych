257aab2becb9d860038e054967e8055930298414
Daniel Sikora <danielsikora100@gmail.com>
2023-12-07 09:32:56 +0100

    dodanie home

---
+++ src/components/Home.js
@@ -0,0 +1,43 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBarComponent from "./NavBarComponent";
+import Cards from "./Cards";
+import Footer from "./Footer";
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+function Home() {
+
+
+    const [films, setFilms] = useState([])
+    const getFilms = () => {
+        axios.get('http://localhost:3001/api/posts')
+            .then((restore) => {
+                setFilms(restore.data)
+            })
+    }
+
+    useEffect(() => {
+        getFilms()
+    }, [])
+
+
+    return (
+        <>
+
+            <NavBarComponent></NavBarComponent>
+            <div className={'home'}>
+                <Cards films={films} style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}></Cards>
+                <Footer></Footer>
+            </div>
+            
+
+
+        </>
+    );
+}
+
+export default Home;
+
+
+
+
